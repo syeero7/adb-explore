1ce6a32ccd9411cd7e1280781527bc00ce3fc1ee
emal <emalhasitha@proton.me>
2026-07-24 05:42:24 +0530

    feat: display directory entries in a table

--- frontend/src/components/Explore.svelte
+++ frontend/src/components/Explore.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
-  import { getEntries, directory } from "@/lib/fs.svelte";
+  import type { main } from "@wails/go/models";
+  import { getEntries, directory, useIsStorageDir } from "@/lib/fs.svelte";
   import Header from "./Header.svelte";
+
+  let selected: string[] = $state([]);
+  const isStorageDir = useIsStorageDir().value;
+
+  const isSymlink = (mode: number) => (mode & 0xf000) === 0xa000;
 </script>
 
 <Header />
@@ -9,13 +15,38 @@
   <p>Loading...</p>
 {:then data}
   {#if Array.isArray(data)}
-    {#each data as entry}
-      <pre>{JSON.stringify(entry)}</pre>
-    {/each}
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>Name</th>
+          <th>Size</th>
+          <th>Date Modified</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {#each data as entry}
+          {@render row(entry)}
+        {/each}
+      </tbody>
+    </table>
   {/if}
 {:catch}
-  <p>failed</p>
+  <p>Failed</p>
 {/await}
+
+{#snippet row(entry: main.Entry)}
+  <tr>
+    <td>
+      <input type="checkbox" value={entry.path} bind:group={selected} disabled={isStorageDir} />
+    </td>
+    <td>{entry.name}</td>
+    <td>{entry.isDir || isSymlink(entry.mode) ? "" : entry.size}</td>
+    {const modified = new Date(entry.lastModified)}
+    <td>{modified.toLocaleDateString()}</td>
+  </tr>
+{/snippet}
 
 <style>
 </style>
